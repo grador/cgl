72d8673c1c51d73e8b20dba27986a998c3891f12
grador <gradator@gmail.com>
2015-01-17 08:29:26 +0700

    with log sessions

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'rvm/capistrano'
 require 'bundler/capistrano'
 set :ssh_options, {:forward_agent => true}
-set :rvm_type, :user
+# set :rvm_type, :user
+set :rvm_type, :system
 set :rvm_ruby_string, "ruby 2.1.3"
 set :application, "Cgl"
 set :domain, "callgoodluck.com"
@@ -9,10 +10,9 @@ set :repository, "https://github.com/grador/cgl/"
 set :default_stage, "production"
 # set :stages, %w(production)
 set :use_sudo, false
-set :user, "root" # нужно предварительно создать юзера на сервере, юзать root"a не стоит
+set :user, "seraf" # нужно предварительно создать юзера на сервере, юзать root"a не стоит
 # set :group, "deployers"
 set :scm, :git
-set :rvm_type, :system
 set :normalize_asset_timestamps, false
 set :scm_verbose, true
 set :rails_env, "production"
@@ -26,19 +26,19 @@ server "31.131.20.142", :web, :app, :db, :primary => true
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-namespace :deploy do
-  desc "Symlink shared config files"
-  task :symlink_config_files do
-    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
-  end
-  desc "Restart Passenger app"
-  task :restart do
-    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-  end
-  after "deploy", "deploy:symlink_config_files"
-  after "deploy", "deploy:restart"
-  after "deploy", "deploy:cleanup"
-end
+# namespace :deploy do
+#   desc "Symlink shared config files"
+#   task :symlink_config_files do
+#     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+#   end
+#   desc "Restart Passenger app"
+#   task :restart do
+#     run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+#   end
+#   after "deploy", "deploy:symlink_config_files"
+#   after "deploy", "deploy:restart"
+#   after "deploy", "deploy:cleanup"
+# end
 
 
 
